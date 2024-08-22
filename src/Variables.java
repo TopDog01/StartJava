78784820958e27fdda397b4fa9d2d1a18a3f9c8c
Yuri Konev <crazy_clown93@mail.ru>
2024-08-22 19:39:11 +0300

    Variables

--- src/Variables.java
+++ src/Variables.java
@@ -1,7 +1,16 @@
 public class Variables {
     private static void main(String[] args) {
-        int myInt;
-        myInt = 10;
+        int myInt = 557;
+        short myShort = 3566;
+        long myLong = 26246286;
+
+        double myDouble = 3.14159;
+        float myFloat = 1.61803f;
+
+        char myChar = 'A';
+
+        boolean myBoolean = true;
+        byte myByte = -1;
         System.out.println(myInt);
     }
 }
