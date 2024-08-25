c5b35bd28913193fb912c1feb304d54e8fdca01c
Yuri Konev <crazy_clown93@mail.ru>
2024-08-25 21:39:45 +0300

    ForLoop

---
+++ src/ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 10; i >=0; i = i - 3) {
+            System.out.println("Hello " + i);
+        }
+    }
+}
