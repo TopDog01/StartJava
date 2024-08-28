61d8822e461c26028a655ac57f31c986a4f5ee7b
Yuri Konev <crazy_clown93@mail.ru>
2024-08-28 20:52:13 +0300

    If

---
+++ src/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args) {
+        int myInt = 5;
+       if (myInt<10){
+           System.out.println("Да, верно!");
+       }else if (myInt<20){
+           System.out.println("Нет, неверно!");
+       //}else{
+       //    System.out.println("Не один из предыдущих случаев");
+       }
+    }
+}
