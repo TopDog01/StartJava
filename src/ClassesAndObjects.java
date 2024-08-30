f301cb9cce21a3b7387acd669321932aab17318d
Yuri Konev <crazy_clown93@mail.ru>
2024-08-30 22:07:47 +0300

    ClassesAndObjects

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,15 +3,20 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "John";
         person1.age = 30;
-        person1.speak();
+    //    person1.speak();
+        int year1 = person1.CalculateYearsToRetirement();
+        System.out.println("Вам " + year1 + " лет до пенсии");
+
 
         Person person2 = new Person();
         person2.name = "Alice";
         person2.age = 25;
     //    person2.speak();
-        person2.sayHello();
+    //    person2.sayHello();
     //    System.out.println("Person 1: Name - " + person1.name + ", Age - " + person1.age);
     //    System.out.println("Person 2: Name - " + person2.name + ", Age - " + person2.age);
+        int year2 = person2.CalculateYearsToRetirement();
+        System.out.println("Вам " + year2 + " лет до пенсии");
     }
 }
 class Person {
@@ -20,6 +25,12 @@ class Person {
     //2. Действия, которые он может совершать(методы)
     String name;
     int age;
+
+
+    int CalculateYearsToRetirement(){
+        int years = 65-age;
+        return years;
+    }
 
     void speak(){
         for(int i = 0; i < 3; i++){
